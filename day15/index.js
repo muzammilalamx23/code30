941af4e0092ce3b2bf14529e1ac808e26a8d233c
Muzammil Alam <166637945+muzammilalamx23@users.noreply.github.com>
2026-01-22 22:31:48 +0530

    Updated the day 15

--- day15/index.js
+++ day15/index.js
@@ -1,1 +1,18 @@
-cd 
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+// Home route
+app.get('/', (req, res) => {
+  res.send('Welcome to my first Express server!');
+});
+
+// API route
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from API!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});

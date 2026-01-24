3e30d832e4a440aa2de3665976e6ff3af7090188
Muzammil Alam <166637945+muzammilalamx23@users.noreply.github.com>
2026-01-25 01:33:30 +0530

    Day 17 mongo setup

---
+++ day17/backend/server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+
+// Connect database
+connectDB();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});

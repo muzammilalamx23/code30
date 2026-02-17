412b7b3bfafa4cbcb84a0b3a31701203e0552d59
Muzammil Alam <166637945+muzammilalamx23@users.noreply.github.com>
2026-02-17 23:16:02 +0530

    Day 20

---
+++ day20/backend/server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
+
+const app = express();
+app.use(express.json());
+
+// connect DB
+connectDB();
+
+// routes
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
